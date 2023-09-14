63f1516ff8a778f595151f53689f206e143d7d17
elleinadseer <126515415+elleinadseer@users.noreply.github.com>
2023-09-14 21:11:57 +0100

    fixed site container error, edited filter names

--- client/src/components/Pages/PropertyFilter.js
+++ client/src/components/Pages/PropertyFilter.js
@@ -15,7 +15,7 @@ export default function PropertyFilter() {
           price
           <p className="priceSym">£££</p>
           <select id="prices">
-            <option value="prices" selected>-- Prices --</option>
+            <option value="prices" selected>Prices</option>
             <option value="less">Less than £99,000</option>
             <option value="1">£100,000</option>
             <option value="2">£200,000</option>
@@ -31,7 +31,7 @@ export default function PropertyFilter() {
           beds
         <img src="https://imgur.com/bvlccTM.png" height="80" width="80" alt="bed"></img>
         <select id="bedrooms">
-            <option value="bedrooms" selected>-- Bedrooms --</option>
+            <option value="bedrooms" selected>Bedrooms</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -45,7 +45,7 @@ export default function PropertyFilter() {
           type
         <img src="https://i.imgur.com/TIlKLdj.png" height="80" width="80" alt="property type"></img>
         <select id="properties">
-            <option value="property" selected>-- Properties --</option>
+            <option value="property" selected>Properties</option>
             <option value="flat">Flat</option>
             <option value="cottage">Cottage</option>
             <option value="bungalow">Bungalow</option>
@@ -59,7 +59,7 @@ export default function PropertyFilter() {
           bathroom
         <img src="https://i.imgur.com/J23J5au.png" height="80" width="80" alt="bathrooms"></img>
         <select id="bathroom">
-            <option value="bathroom" selected>-- Bathrooms --</option>
+            <option value="bathroom" selected>Bathrooms</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -72,7 +72,7 @@ export default function PropertyFilter() {
           pets
         <img src="https://i.imgur.com/bvR5Faw.png" height="80" width="80" alt="paw"></img>
         <select id="bathroom">
-            <option value="bathroom" selected>-- Pet-Friendly --</option>
+            <option value="bathroom" selected>Pet-Friendly</option>
             <option value="yes">Yes</option>
             <option value="no">No</option>
             </select>
