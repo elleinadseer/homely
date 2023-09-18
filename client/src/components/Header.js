4a9b08580a41590ed3d9a8e4a7dd9e4c4d0a3a22
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-09-18 03:28:09 +0100

    Added Log Out and Favourites buttons Conditionally rendered based on logged in state

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Auth from '../utils/auth';
 
 export default function Header() {
+  const isLoggedin = Auth.loggedIn();
+
+  const handleLogout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
   return (
     <div>
-      <span className='center'>
-      <Link to="/"><img src="https://i.imgur.com/EfZzC5F.png" alt="homely logo"></img></Link>
-      <div className="signLogLinks">
-        <Link to="/signup">Sign up</Link> or <Link to="/login">log in</Link> to favourite properties
-      </div>
+      <span className="center">
+        <Link to="/">
+          <img src="https://i.imgur.com/EfZzC5F.png" alt="homely logo"></img>
+        </Link>
+        <div className="signLogLinks">
+          {isLoggedin ? (
+            <>
+              <Link to="/favorites">Favorites</Link> |{' '}
+              <Link onClick={handleLogout}>Log out</Link>
+            </>
+          ) : (
+            <>
+              <Link to="/signup">Sign up</Link> or{' '}
+              <Link to="/login">log in</Link> to favourite properties
+            </>
+          )}
+        </div>
       </span>
     </div>
   );
