19b4a75d23af301aa4e38964ba57b677511169f3
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-09-18 05:10:44 +0100

    Added page for displaying favourite properties

---
+++ client/src/components/Pages/Favourites.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useQuery } from '@apollo/client'
+import { QUERY_ME } from '../../utils/queries/propertyQueries'
+import { Link } from 'react-router-dom'
+
+const FavoritesPage = () => {
+  // Use the GraphQL query to fetch the user's favorite properties
+  const { loading, error, data } = useQuery(QUERY_ME)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+
+  const favoriteProperties = data.me.savedProperties
+
+  return (
+    <div className="propertyListings">
+      {/* Display the user's favorite properties */}
+      {favoriteProperties.map((property) => (
+        <div key={property._id}>
+          {/* Display property details as needed */}
+          <div className="PropertyTile">
+            <Link to={`/property/${property._id}`}>
+              {property?.images?.map((image, index) => (
+                <img
+                  key={index}
+                  src={image.image}
+                  height="250"
+                  width="400"
+                  alt="property"
+                />
+              ))}
+            </Link>
+            <div className="propertyText">
+              <h2>
+                £{property.price} {property.rent === true ? 'pcm' : ''}
+              </h2>
+              <p>
+                {property.beds} Bedroom {property?.propertyType?.name} For{' '}
+                {property.rent === true ? 'Rent' : 'Sale'}
+              </p>
+              <p>
+                {property.address}, {property.city}, {property.postcode}
+              </p>
+              <span className="imgSpan">
+                <img
+                  src="https://i.imgur.com/J23J5au.png"
+                  height="25"
+                  width="25"
+                  alt="bathrooms"
+                ></img>{' '}
+                Bathrooms: {property.baths}
+              </span>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default FavoritesPage
